c1538fde75df66f3fb785228f0cb3da6aabf4b8b
resiato <resiatolaitete@gmail.com>
2021-07-27 11:39:06 -0700

    Functions to print characters

--- _printf.c
+++ _printf.c
@@ -11,19 +11,8 @@ static int (*check_specs(const char *format))(va_list)
 {
 	unsigned int i;
 	print_t p[] = {
-		{"c", print_c},
-		{"s", print_s},
-		{"i", print_i},
-		{"d", print_d},
-		{"u", print_u},
-		{"b", print_b},
-		{"o", print_o},
-		{"x", print_x},
-		{"X", print_X},
-		{"p", print_p},
-		{"S", print_S},
-		{"r", print_r},
-		{"R", print_R},
+		{"c", print_character},
+		{"s", print_string},
 		{NULL, NULL}
 	};
 
@@ -64,7 +53,7 @@ int _printf(const char *format, ...)
 		f = check_specs(&format[i + 1]);
 		if (f != NULL)
 		{
-			count += f(valist);
+			count += f(args);
 			i += 2;
 			continue;
 		}
